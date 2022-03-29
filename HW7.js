2229af201729639285305e38f0a520c5814caa89
stubbs497 <c.jameson.s1002@gmail.com>
2022-03-29 11:20:27 -0400

    Update HW7.js

--- HW7.js
+++ HW7.js
@@ -1,9 +1,3 @@
-function doExternalFileThing(elt) {
-    elt.innerHTML = 'Done from an external file!';
-    elt.disabled = true;
-}
-
-/* -------------- Conditional ------------------------------*/
 function findTimeOfDay(button) {
     let parentElt = button.parentElement;
     let time = (new Date()).getHours();
@@ -18,32 +12,6 @@ function findTimeOfDay(button) {
     }
     else {  
         contents = contents + 'Evening';
-    }
-
-    parentElt.innerHTML = contents;
-}
-
-function findDayOfWeekSwitch(button) {
-    let parentElt = button.parentElement;
-    let daynum = (new Date()).getDay(); // 0-6
-    
-    contents = 'Today is a ';
-
-    switch (daynum) {
-        case 0: contents = contents + 'Sunday'; 
-                break;
-        case 1: contents = contents + 'Monday'; 
-                break;
-        case 2: contents = contents + 'Tuesday'; 
-                break;
-        case 3: contents = contents + 'Wednesday'; 
-                break;
-        case 4: contents = contents + 'Thursday'; 
-                break;
-        case 5: contents = contents + 'Friday'; 
-                break;
-        default: contents = contents + 'Saturday'; 
-                 break;
     }
 
     parentElt.innerHTML = contents;
