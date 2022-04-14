eca69697f3cb6f213b3a0dfeec93431d955f8261
stubbs497 <c.jameson.s1002@gmail.com>
2022-04-14 12:19:29 -0400

    Update HW8.js

--- HW8.js
+++ HW8.js
@@ -1,3 +1,9 @@
 function validForm(formElt) {
-    return false;
-    }
+    console.log('validForm called');
+    valid = true;
+
+    valid = validButtons(formElt.querySelector('fieldset:nth-of-type(2)'),
+                        'input[type=checkbox]', 'p > span', 2);
+    console.log('First validButtons: ' + valid);
+    return valid
+} 
